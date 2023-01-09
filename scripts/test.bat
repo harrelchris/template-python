dd574bd7cb2b1b22cd8feb40c1330373397f92b1
harrelchris <24297899+harrelchris@users.noreply.github.com>
2023-01-09 17:23:12 -0600

    refactor test script

--- scripts/test.bat
+++ scripts/test.bat
@@ -5,10 +5,7 @@ if not defined VIRTUAL_ENV (
 	call .venv\Scripts\activate
 )
 
-:: Run pytest and generate report
-python -m pytest --html=.reports/pytest/index.html --self-contained-html
-
-:: Run coverage and generate report
+:: Run Pytest with Coverage and generate reports
 coverage erase
-coverage run -m pytest
-coverage html -d .reports/coverage --fail-under 100
+coverage run -m pytest --html=.reports/pytest/index.html --self-contained-html
+coverage html
